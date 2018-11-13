353e2e8c0ff0e118463b319ce71c8ca6788a32e0
pablo <pcoronel@untref.edu.ar>
2018-11-13 18:42:17 -0300

    ver request

--- app/Http/Controllers/AlfajorsController.php
+++ app/Http/Controllers/AlfajorsController.php
@@ -15,6 +15,35 @@ class AlfajorsController extends Controller
      */
     public function imagenUpload(Request $request)
     {
-        dd($request);
+        return $request->all();
+        // if ($request->hasFile('imagen')) {
+        //     return response()->json(Response::HTTP_OK);
+        // }else{
+        //     return response()->json(Response::HTTP_NOT_FOUND);
+        // }
+        // if ($request->hasFile('imagen')) {
+            // $fileData = \File::get($file->path());
+
+            // $fileName = $this->createFilename($file);
+            // Group files by mime type
+            // $mime = str_replace('/', '-', $file->getMimeType());
+            // Group files by the date (week
+            // $dateFolder = date("Y-m-W");
+
+            // Build the file path
+            // $filePath  = "upload/{$mime}/{$dateFolder}/";
+            // $finalPath = public_path("/");
+            // $request->file('imagen')->move('/', 's.jpg');
+// $finalPath = storage_path("app/" . $filePath);
+
+            // move the file name (Sube al hosting)
+            // $file->move($finalPath, $fileName);
+
+            // return response()->json(Response::HTTP_OK);
+        // }else{
+            return response()->json(Response::HTTP_NOT_FOUND);
+        // }
+
     }
+
 }
