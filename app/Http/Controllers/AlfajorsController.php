98359752c7f27f597a8b2cca32222ee0660a5377
pablo <pcoronel@untref.edu.ar>
2018-11-15 17:39:12 -0300

    ver request

--- app/Http/Controllers/AlfajorsController.php
+++ app/Http/Controllers/AlfajorsController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Http\Controllers;
+header('Access-Control-Allow-Origin: *');
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,33 +16,42 @@ class AlfajorsController extends Controller
      */
     public function imagenUpload(Request $request)
     {
+        
+        // if ($request->hasFile('imagen')) {
+            //     return response()->json(Response::HTTP_OK);
+            // }else{
+                //     return response()->json(Response::HTTP_NOT_FOUND);
+                // }
+                // if ($request->hasFile('imagen')) {
+                    // $fileData = \File::get($file->path());
+                    
+                    // $fileName = $this->createFilename($file);
+                    // Group files by mime type
+                    // $mime = str_replace('/', '-', $file->getMimeType());
+                    // Group files by the date (week
+                    // $dateFolder = date("Y-m-W");
+                    
+                    // Build the file path
+                    // $filePath  = "upload/{$mime}/{$dateFolder}/";
+                    // $finalPath = public_path("/");
+                    // $request->file('imagen')->move('/', 's.jpg');
+                    // $finalPath = storage_path("app/" . $filePath);
+                    
+                    // move the file name (Sube al hosting)
+                    // $file->move($finalPath, $fileName);
+                    
+                    // return response()->json(Response::HTTP_OK);
+                    // }else{
+                        // return response()->json(Response::HTTP_NOT_FOUND);
+                        // }
+                        
+                        
         return $request->all();
+
         // if ($request->hasFile('imagen')) {
-        //     return response()->json(Response::HTTP_OK);
+        //     dd('si');
         // }else{
-        //     return response()->json(Response::HTTP_NOT_FOUND);
-        // }
-        // if ($request->hasFile('imagen')) {
-            // $fileData = \File::get($file->path());
-
-            // $fileName = $this->createFilename($file);
-            // Group files by mime type
-            // $mime = str_replace('/', '-', $file->getMimeType());
-            // Group files by the date (week
-            // $dateFolder = date("Y-m-W");
-
-            // Build the file path
-            // $filePath  = "upload/{$mime}/{$dateFolder}/";
-            // $finalPath = public_path("/");
-            // $request->file('imagen')->move('/', 's.jpg');
-// $finalPath = storage_path("app/" . $filePath);
-
-            // move the file name (Sube al hosting)
-            // $file->move($finalPath, $fileName);
-
-            // return response()->json(Response::HTTP_OK);
-        // }else{
-            return response()->json(Response::HTTP_NOT_FOUND);
+        //     dd('no');
         // }
 
     }
